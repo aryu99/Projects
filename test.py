6042f07da3865ecaaa6ca5270213af2fdef9ce9d
Aryu <aryamansinghsamyal@gmail.com>
2020-04-05 02:27:32 +0530

    4th may

---
+++ test.py
@@ -0,0 +1,18 @@
+import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+import math
+
+val = 0
+
+def start():
+	global val
+	if start() = true:
+		val = 2
+	print (val)
+
+
+
+
+frame = simplegui.create_frame("test", 200, 200)
+frame.add_button("start", start)
+
+frame.start()
